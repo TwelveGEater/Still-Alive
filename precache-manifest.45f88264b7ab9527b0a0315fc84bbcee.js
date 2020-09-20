f52748abd2a57b3f0ace4eed03785a7a4efbcffd
dronkfc@gmail.com <dronkfc@gmail.com>
2020-09-20 13:06:12 +0300

    Updates

---
+++ precache-manifest.45f88264b7ab9527b0a0315fc84bbcee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287ee583a8a7650861781bfd330f4f44",
+    "url": "/Still-Alive/index.html"
+  },
+  {
+    "revision": "2f95be1c9f873eac2f11",
+    "url": "/Still-Alive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1aa24341c0cf8466af9c",
+    "url": "/Still-Alive/static/js/2.f110687e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Still-Alive/static/js/2.f110687e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f95be1c9f873eac2f11",
+    "url": "/Still-Alive/static/js/main.96271c25.chunk.js"
+  },
+  {
+    "revision": "7fde86bd0fad7dfc22d7",
+    "url": "/Still-Alive/static/js/runtime-main.36e8569a.js"
+  },
+  {
+    "revision": "a7b42e093dfe4a1a77e3b5ac4703a21f",
+    "url": "/Still-Alive/static/media/enemy.a7b42e09.gif"
+  },
+  {
+    "revision": "72e67747b85fb10918eb5e2f7e4f8c04",
+    "url": "/Still-Alive/static/media/fireball.72e67747.png"
+  },
+  {
+    "revision": "8d08d77540cc1f999292779e1bd39f94",
+    "url": "/Still-Alive/static/media/magic-spell.8d08d775.png"
+  }
+]);
